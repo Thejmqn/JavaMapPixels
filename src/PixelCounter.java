f9e6c7d610e5829377852bb73f84065f1fec1888
TheJmqn <jman4461@gmail.com>
2024-12-19 22:53:22 -0500

    Allow for slight difference in claims colors

--- src/PixelCounter.java
+++ src/PixelCounter.java
@@ -21,7 +21,7 @@ public class PixelCounter {
             for (int y = 0; y < image.getHeight(); y++) {
                 Color pixelColor = getPixelColor(x, y);
                 for (Claim claim : claims) {
-                    if (claim.getColor().equals(pixelColor))
+                    if (areColorsClose(claim.getColor(), pixelColor))
                         claim.setPixelCount(claim.getPixelCount() + 1);
                 }
             }
@@ -35,5 +35,13 @@ public class PixelCounter {
         int green = (color & 0x0000ff00) >> 8;
         int blue = (color & 0x000000ff);
         return new Color(red, green, blue);
+    }
+
+    private boolean areColorsClose(Color color1, Color color2) {
+        final int MAX_DIFFERENCE = 5; // set to 1 for same color
+        return Math.abs(color1.getRed() - color2.getRed()) <= MAX_DIFFERENCE &&
+                Math.abs(color1.getGreen() - color2.getGreen()) <= MAX_DIFFERENCE &&
+                Math.abs(color1.getBlue() - color2.getBlue()) <= MAX_DIFFERENCE &&
+                Math.abs(color1.getAlpha() - color2.getAlpha()) <= MAX_DIFFERENCE;
     }
 }
